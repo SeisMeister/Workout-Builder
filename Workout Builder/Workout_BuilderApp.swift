fdaae0a14c920396ba5ada3c80b8d71ab414c5b9
Cesar Fernandez <CesarFernandez2@live.com>
2024-08-15 08:37:20 -0600

    Initial Commit

---
+++ Workout Builder/Workout_BuilderApp.swift
@@ -0,0 +1,32 @@
+//
+//  Workout_BuilderApp.swift
+//  Workout Builder
+//
+//  Created by Cesar Fernandez on 8/15/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Workout_BuilderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
