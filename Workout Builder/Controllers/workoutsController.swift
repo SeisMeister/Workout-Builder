6677adf700db1f5528f4c9ad7db55a643e2b5a48
Cesar Fernandez <CesarFernandez2@live.com>
2024-08-30 10:32:28 -0600

    Began workoutsController

---
+++ Workout Builder/Controllers/workoutsController.swift
@@ -0,0 +1,20 @@
+//
+//  workoutsController.swift
+//  Workout Builder
+//
+//  Created by Cesar Fernandez on 8/30/24.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+
+class workoutsController {
+    static let shared = workoutsController()
+    
+    func createNewWorkout(workoutName: String, sets: Int, reps: Int) {
+        
+    }
+}
+
